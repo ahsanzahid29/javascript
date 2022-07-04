586c479d1eea63714cba8d009955bd497c44527e
ahsan zahid <ahsanzahid29@hotmail.com>
2022-07-04 06:24:16 +0500

    Basic javascript is implemented

---
+++ js/index.js
@@ -0,0 +1,13 @@
+var a=[1,2,'hamid','ahsan'];
+console.log(a);
+//console.log(a);
+//console.log(a.length)//get length of array
+//a.push(12) //add value at last index
+//console.log(a);
+var b=a.toString();
+console.log(b);
+
+
+
+
+
