0352afdd38acd01516b2db339dcc174b2e9fa23d
ahsan zahid <ahsanzahid29@hotmail.com>
2022-07-05 06:34:00 +0500

    Tutorial completed

--- js/index.js
+++ js/index.js
@@ -1,11 +1,51 @@
-var a=[1,2,'hamid','ahsan'];
-console.log(a);
-//console.log(a);
-//console.log(a.length)//get length of array
-//a.push(12) //add value at last index
-//console.log(a);
-var b=a.toString();
-console.log(b);
+// var str= "Hamid is living in brisbane. Hamid is a gym person";
+// // console.log(str.length);//length of string
+// // console.log(str.indexOf("brisbane"));
+// // console.log(str.lastIndexOf('Hamid'));
+// // 
+// console.log(str.replace("Hamid","Ahsan"));
+
+//Date function in js
+// let myDate= new Date();
+// console.log(myDate);
+// console.log(myDate.getMinutes())
+// For Loop 1,2,3,4,5,6,7,8,9,10
+var arr=[1,2,5,"hamid",true];
+//ForEach
+arr.forEach(function(hamid){
+    if(hamid==2){
+
+    }else{
+        console.log(hamid)
+
+    }
+   
+})
+
+
+// for(var i=0; i<arr.length;i++){
+//     if(arr[i]==2){
+
+//     }
+//     else{
+//         console.log(arr[i]);
+
+//     }
+    
+// }
+// while loop
+// var i=0;
+// while(i<arr.length){
+//     console.log(arr[i]);
+//     i++;
+// }
+// do while loop
+// var i=0;
+// do{
+//     console.log(arr[i]);
+//     i++;
+// }while(i<arr.length)
+
 
 
 
