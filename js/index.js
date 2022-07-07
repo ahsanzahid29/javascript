5ae8ebf75058b4cd25c7e46c37aecd3ba941bf20
ahsan zahid <ahsanzahid29@hotmail.com>
2022-07-07 06:11:46 +0500

    Code added

--- js/index.js
+++ js/index.js
@@ -1,50 +1,9 @@
-// var str= "Hamid is living in brisbane. Hamid is a gym person";
-// // console.log(str.length);//length of string
-// // console.log(str.indexOf("brisbane"));
-// // console.log(str.lastIndexOf('Hamid'));
-// // 
-// console.log(str.replace("Hamid","Ahsan"));
-
-//Date function in js
-// let myDate= new Date();
-// console.log(myDate);
-// console.log(myDate.getMinutes())
-// For Loop 1,2,3,4,5,6,7,8,9,10
-var arr=[1,2,5,"hamid",true];
-//ForEach
-arr.forEach(function(hamid){
-    if(hamid==2){
-
-    }else{
-        console.log(hamid)
-
-    }
-   
+hamid.addEventListener('click',function(){
+    alert('button is clicked');
 })
 
 
-// for(var i=0; i<arr.length;i++){
-//     if(arr[i]==2){
 
-//     }
-//     else{
-//         console.log(arr[i]);
-
-//     }
-    
-// }
-// while loop
-// var i=0;
-// while(i<arr.length){
-//     console.log(arr[i]);
-//     i++;
-// }
-// do while loop
-// var i=0;
-// do{
-//     console.log(arr[i]);
-//     i++;
-// }while(i<arr.length)
 
 
 
